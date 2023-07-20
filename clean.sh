57bbda7ae31afea07323579e04f5b4d40b3f6a67
Mr4 <dungdv@sabitech.vn>
2023-07-20 11:15:55 +0700

    Add - run.sh - clean.sh

---
+++ clean.sh
@@ -0,0 +1,14 @@
+composer install
+npm install
+npm run build
+composer dumpautoload
+php artisan clear-compiled
+php artisan route:clear
+php artisan event:clear
+php artisan config:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan migrate:fresh --seed
+php artisan vendor:publish --all
+php artisan storage:link
+php artisan serve
